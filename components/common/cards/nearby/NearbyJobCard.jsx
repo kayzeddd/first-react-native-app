1677753b5ab3b2c85af9422ae3936cb43cbb516a
kayzed <kayzed98@gmail.com>
2023-05-01 19:48:30 -0400

    Add progress code

--- components/common/cards/nearby/NearbyJobCard.jsx
+++ components/common/cards/nearby/NearbyJobCard.jsx
@@ -1,13 +1,36 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity, Image } from 'react-native'
 
 import styles from './nearbyjobcard.style'
 
-const NearbyJobCard = () => {
+import { checkImageURL } from '../../../../utils'
+
+const NearbyJobCard = ({ job, handleNavigate }) => {
   return (
-    <View>
-      <Text>NearbyJobCard</Text>
-    </View>
+    <TouchableOpacity
+      style={styles.container}
+      onPress={handleNavigate}
+    >
+      <TouchableOpacity style={styles.logoContainer}>
+        <Image 
+          source={{ uri: checkImageURL(job.employer_logo) 
+            ? job.employer_logo
+            : 'https://t3.ftcdn.net/jpg/05/25/17/98/360_F_525179852_dPo0NiSY6GsguULdiHAH4X2mFIuk9HQ2.jpg'
+          }}
+          resizeMode="contain"
+          style={styles.logoImage}
+        />
+      </TouchableOpacity>
+
+      <View
+        style={styles.textContainer}
+      >
+        <Text style={styles.jobName} numberOfLines={1}>
+          {job.job_title}
+        </Text>
+        <Text style={styles.jobType}>{job.job_employment_type}</Text>
+      </View>
+    </TouchableOpacity>
   )
 }
 
--- hook/useFetch.js
+++ hook/useFetch.js
@@ -1,6 +1,53 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const useFetch = () =>{
-    
+// import { JSEARCH_API_KEY } from '@env' //not working
+// const jsearchApiKey = JSEARCH_API_KEY;
+// require("dotenv").config();
+// const { JSEARCH_API_KEY } = process.env;
+
+const dontStealPls = "a74bf264f1msh5a71a8e64ba0f42p1848e5jsn04ffb7a367ec"
+
+const useFetch = (endpoint, query) =>{
+    const [data, setData] = useState([])
+    const [isLoading, setIsLoading] = useState(false)
+    const [error, setError] = useState(null)
+
+    const options = {
+        method: 'GET',
+        url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+        headers: {
+          'X-RapidAPI-Key': dontStealPls,
+          'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
+        },
+        params: { ...query }
+      };
+
+    const fetchData = async () => {
+        setIsLoading(true);
+
+        try {
+            const response = await axios.request(options)
+            setData(response.data.data)
+            setIsLoading(false)
+        } catch (error) {
+            setError(error);
+            alert("There is an error")
+        } finally {
+            setIsLoading(false)
+        }
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, [])
+
+    const refetch = () => {
+        setIsLoading(true);
+        fetchData();
+    }
+
+    return { data, isLoading, error, refetch}
 }
+
+export default useFetch

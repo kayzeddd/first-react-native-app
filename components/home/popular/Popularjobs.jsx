b6503c22e771c4cf23b08e700e0e0ebbc5415f7f
kayzed <kayzed98@gmail.com>
2023-05-01 21:54:55 -0400

    Add code

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -12,6 +12,13 @@ const Popularjobs = () => {
   const router = useRouter();
   const { data, isLoading, error } = useFetch('search', {query:'Web Developer', num_pages: 1})
 
+  const [selectedJob, setSelectedJob] = useState();
+
+  const handleCardPress = (item) => {
+    router.push(`/job-details/${item.job_id}`);
+    setSelectedJob(item.job_id);
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
@@ -32,6 +39,8 @@ const Popularjobs = () => {
             renderItem={({ item }) => (
               <PopularJobCard
                 item={item}
+                selectedJob={selectedJob}
+                handleCardPress={handleCardPress}
               />
             )}
             keyExtractor={item => item?.job_id}
